c9f317384b5fc2e9f98ba4acd0b0193304f23620
GennyTheDev <genevievemomoh@gmail.com>
2025-06-29 00:08:26 +0100

    feat: update .gitignore to include .env.example and modify rootDomain logic in utils

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,7 +4,9 @@ import { twMerge } from "tailwind-merge";
 export const protocol =
   process.env.NODE_ENV === "production" ? "https" : "http";
 export const rootDomain =
-  process.env.NEXT_PUBLIC_ROOT_DOMAIN || "localhost:3000";
+  process.env.VERCEL_URL ||
+  process.env.NEXT_PUBLIC_ROOT_DOMAIN ||
+  "localhost:3000";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
